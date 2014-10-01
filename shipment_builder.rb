850f237f571abc81f617189a8eb22270375ac7d1
Nickolas Means <nick@heliumsyndicate.com>
2014-10-01 07:29:47 -0500

    Reduce collaborators/Demeter violations and first passing test

--- shipment_builder.rb
+++ shipment_builder.rb
@@ -1,3 +1,5 @@
+require_relative 'shipment'
+
 class ShipmentBuilder
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # Determine the groups in which items will be shipped. This is necessary for
@@ -5,25 +7,20 @@ class ShipmentBuilder
   # drop-shipped from a vendors warehouse.
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  def initialize(order_line_items, consolidate = false)
-    build_shipments(order_line_items, consolidate)
-  end
-
-  def build_shipments( order_line_items, consolidate)
+  def build_shipments( order_line_items, consolidate = false)
     shipments = []
 
     unless consolidate
       # Build a hash of line items with associated data we can use without having to re-query the database for all the iterations.
       line_items = []
       order_line_items.each do |order_line_item|
-        item = order_line_item.item_id ? Item.find(order_line_item.item_id, :include => :product) : UsedItem.find(order_line_item.used_item_id)
-        ship_status_symbol = item.shipping_status_symbol_for_quantity(order_line_item.quantity)
+        ship_status_symbol = order_line_item.ship_status
         ship_status_symbol = :in_stock if [:closeout_instock,:from_stock_only_instock].include?(ship_status_symbol)
         line_items << OpenStruct.new( :ship_status_symbol => ship_status_symbol,
-                                      :store_id => item.instance_of?(Item) ? item.product.store_id : 1,
-                                      :vendor_id => item.instance_of?(Item) ? item.product.vendor_id : nil,
+                                      :store_id => order_line_item.store_id,
+                                      :vendor_id => order_line_item.vendor_id,
                                       :line_item => order_line_item,
-                                      :drop_shippable => item.drop_shippable?,
+                                      :drop_shippable => order_line_item.drop_shippable?,
                                       :consolidatable => false)
       end
 
@@ -55,8 +52,7 @@ class ShipmentBuilder
       end
     else
       order_line_items.each do |order_line_item|
-        item = order_line_item.item_id ? Item.find(order_line_item.item_id, :include => :product) : UsedItem.find(order_line_item.used_item_id)
-        create_group_if_necessary_and_insert( shipments, :consolidated, item.store_id, item.store_id, order_line_item)
+        create_group_if_necessary_and_insert( shipments, :consolidated, order_line_item.store_id, order_line_item.store_id, order_line_item)
       end
     end
 
@@ -66,14 +62,14 @@ class ShipmentBuilder
   def create_group_if_necessary_and_insert( shipments, key, store_id, shipper_id, order_line_item)
     found = false
     shipments.each do |shipment|
-      if shipment.shipment_type == shipment.shipment_type_from_symbol( key ) && shipment.shipper_id == shipper_id && shipment.store_id == store_id
+      if shipment.shipment_type == key && shipment.shipper_id == shipper_id
         shipment.line_items << order_line_item
         found = true
       end
     end
     if found == false
       shipment = Shipment.new
-      shipment.shipment_type = shipment.shipment_type_from_symbol( key )
+      shipment.shipment_type = key
       shipment.shipper_id = shipper_id
       shipment.store_id = store_id
       shipment.line_items << order_line_item
--- shipment_builder_spec.rb
+++ shipment_builder_spec.rb
@@ -1,16 +1,17 @@
 require_relative 'spec_helper'
 require_relative 'shipment_builder'
+require_relative 'mock_order_line_item'
 require 'ostruct'
 
 describe ShipmentBuilder do
 
   it "returns a single consolidated shipment when consolidate is true" do
-    sb = ShipmentBuilder.new([mock_order_line_item, mock_order_line_item], true)
-    sb.length.must_equal 1
+    shipments = ShipmentBuilder.new.build_shipments([mock_order_line_item, mock_order_line_item], true)
+    shipments.length.must_equal 1
   end
 
 end
 
-def mock_order_line_item
-  OpenStruct.new()
+def mock_order_line_item(ship_status = :in_stock, store_id = 1)
+  MockOrderLineItem.new(ship_status: ship_status, store_id: store_id)
 end
