e69998db117cecae3d99c847fe5447e19474920e
Nickolas Means <nick@heliumsyndicate.com>
2014-09-30 22:11:21 -0500

    First failing test

--- shipment_builder_spec.rb
+++ shipment_builder_spec.rb
@@ -3,11 +3,14 @@ require_relative 'shipment_builder'
 require 'ostruct'
 
 describe ShipmentBuilder do
-  it "exists" do
-    assert ShipmentBuilder.new(order_line_item)
+
+  it "returns a single consolidated shipment when consolidate is true" do
+    sb = ShipmentBuilder.new([mock_order_line_item, mock_order_line_item], true)
+    sb.length.must_equal 1
   end
+
 end
 
-def order_line_item
-  OpenStruct.new(:item_id => 1)
+def mock_order_line_item
+  OpenStruct.new()
 end
