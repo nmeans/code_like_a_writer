f3c5e69e90fc26fb54d4858a09c5a00a9aaa5e03
Nickolas Means <nick@heliumsyndicate.com>
2014-10-02 07:23:06 -0500

    Wrap up characterization tests

--- shipment_builder_spec.rb
+++ shipment_builder_spec.rb
@@ -21,6 +21,48 @@ describe ShipmentBuilder do
     shipments.first.shipment_type.must_equal :in_stock
   end
 
+  it "returns multiple shipments when types and vendors differ" do
+    order_line_items = [
+      mock_order_line_item(ship_status: :in_stock),
+      mock_order_line_item(ship_status: :drop_ship, vendor_id: 9),
+      mock_order_line_item(ship_status: :drop_ship, vendor_id: 10),
+      mock_order_line_item(ship_status: :order_in),
+    ]
+    shipments = ShipmentBuilder.new.build_shipments(order_line_items)
+    shipments.length.must_equal 4
+    shipments.count{|s| s.shipment_type == :drop_ship}.must_equal 2
+  end
+
+  it "consolidates drop ship items from the same vendor" do
+    order_line_items = [
+      mock_order_line_item(ship_status: :drop_ship, vendor_id: 9),
+      mock_order_line_item(ship_status: :drop_ship, vendor_id: 9),
+    ]
+    shipments = ShipmentBuilder.new.build_shipments(order_line_items)
+    shipments.length.must_equal 1
+  end
+
+  it "consolidates in_stock to drop_ship if all in_stock items can be consolidated" do
+    order_line_items = [
+      mock_order_line_item(ship_status: :in_stock, vendor_id: 9),
+      mock_order_line_item(ship_status: :drop_ship, vendor_id: 9),
+    ]
+    shipments = ShipmentBuilder.new.build_shipments(order_line_items)
+    shipments.length.must_equal 1
+    shipments.first.shipment_type.must_equal :drop_ship
+  end
+
+  it "does not consolidate in_stock to drop ship if any in_stock items are not consolidatable" do
+    order_line_items = [
+      mock_order_line_item(ship_status: :in_stock, vendor_id: 9),
+      mock_order_line_item(ship_status: :drop_ship, vendor_id: 9),
+      mock_order_line_item(ship_status: :in_stock, vendor_id: 10),
+    ]
+    shipments = ShipmentBuilder.new.build_shipments(order_line_items)
+    shipments.length.must_equal 2
+    shipments.find{|s| s.shipment_type == :drop_ship}.line_items.length.must_equal 1
+  end
+
 end
 
 def mock_order_line_item(overrides = {})
