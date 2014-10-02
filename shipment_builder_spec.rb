152ff679ddb9124127a9487365ab17bcef1119a6
Nickolas Means <nick@heliumsyndicate.com>
2014-10-02 06:42:31 -0500

    Change mock_order_line_item helper interface

--- shipment_builder_spec.rb
+++ shipment_builder_spec.rb
@@ -11,13 +11,14 @@ describe ShipmentBuilder do
   end
 
   it "returns a single in_stock shipment" do
-    shipments = ShipmentBuilder.new.build_shipments([mock_order_line_item(:in_stock), mock_order_line_item(:in_stock)])
+    shipments = ShipmentBuilder.new.build_shipments([mock_order_line_item, mock_order_line_item])
     shipments.length.must_equal 1
     shipments.first.shipment_type.must_equal :in_stock
   end
 
 end
 
-def mock_order_line_item(ship_status = :in_stock, store_id = 1, vendor_id = 1)
-  MockOrderLineItem.new(ship_status: ship_status, store_id: store_id, vendor_id: 1)
+def mock_order_line_item(overrides = {})
+  params = {:ship_status => :in_stock, :store_id => 1, :vendor_id => 1, :drop_shippable => true}.merge(overrides)
+  MockOrderLineItem.new(params)
 end
