91748e49220280c499ee24511a410491fc77ae1c
Nickolas Means <nick@heliumsyndicate.com>
2014-10-02 06:42:52 -0500

    Add drop_shippable to mock_order_line_item

--- mock_order_line_item.rb
+++ mock_order_line_item.rb
@@ -4,4 +4,9 @@ class MockOrderLineItem
   attribute :ship_status, Symbol
   attribute :store_id, Integer
   attribute :vendor_id, Integer
+  attribute :drop_shippable, Boolean
+
+  def drop_shippable?
+    drop_shippable
+  end
 end
