5297212f02bfb4e5f550115f7219af8a1eb2565d
Nickolas Means <nick@heliumsyndicate.com>
2014-10-13 07:32:29 -0500

    Extract ShipmentList value object

--- shipment_builder.rb
+++ shipment_builder.rb
@@ -1,25 +1,21 @@
 require_relative 'shipment'
 
 class ShipmentBuilder
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  # Determine the groups in which items will be shipped. This is necessary for
-  # non-consolidated shipments when some items are out of stock or are being
-  # drop-shipped from a vendors warehouse.
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  attr_reader :line_items, :consolidate_to_single_shipment, :shipments
+  attr_reader :line_items, :consolidate_to_single_shipment
 
   def initialize(line_items, consolidate_to_single_shipment = false)
     @line_items = line_items
     @consolidate_to_single_shipment = consolidate_to_single_shipment
-    @shipments = []
   end
 
   def build_shipments
     optimize_ship_statuses
-    line_items.each { |li| assign_to_shipment(li) }
-
+    assign_items_to_shipments
     return shipments
+  end
+
+  def assign_items_to_shipments
+    line_items.each { |li| shipment_list.assign_to_shipment(li) }
   end
 
   def optimize_ship_statuses
@@ -43,23 +39,40 @@ class ShipmentBuilder
     (in_stock_items.map(&:vendor_id) - drop_ship_items.map(&:vendor_id)).empty?
   end
 
-  def assign_to_shipment(line_item)
-    shipment = find_or_create_shipment(line_item.ship_status, line_item.vendor_id)
-    shipment.line_items << line_item
+  def shipment_list
+    @shipment_list ||= ShipmentList.new
   end
 
-  def find_or_create_shipment(type, shipper_id)
-    find_shipment(type, shipper_id) || create_shipment(type, shipper_id)
+  def shipments
+    shipment_list.shipments
   end
 
-  def find_shipment(type, shipper_id)
-    shipments.find do |shipment|
-      shipment.shipment_type == type && (shipment.shipper_id == shipper_id || shipment.shipment_type != :drop_ship)
+  class ShipmentList
+    attr_reader :shipments
+
+    def initialize
+      @shipments = []
     end
-  end
 
-  def create_shipment(type, shipper_id)
-    shipments << new_shipment = Shipment.new(shipment_type: type, shipper_id: shipper_id, store_id: 1)
-    new_shipment
+    def assign_to_shipment(line_item)
+      shipment = find_or_create_shipment(line_item.ship_status, line_item.vendor_id)
+      shipment.line_items << line_item
+    end
+
+    def find_or_create_shipment(type, shipper_id)
+      find_shipment(type, shipper_id) || create_shipment(type, shipper_id)
+    end
+
+    def find_shipment(type, shipper_id)
+      shipments.find do |shipment|
+        shipment.shipment_type == type && (shipment.shipper_id == shipper_id || shipment.shipment_type != :drop_ship)
+      end
+    end
+
+    def create_shipment(type, shipper_id)
+      shipments << new_shipment = Shipment.new(shipment_type: type, shipper_id: shipper_id, store_id: 1)
+      new_shipment
+    end
+
   end
 end
