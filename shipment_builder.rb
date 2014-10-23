ebafa6e7fcc99e3bdc68eb604d492736449fb661
Nickolas Means <nick@heliumsyndicate.com>
2014-10-22 22:05:56 -0500

    Further cleanup of consolidate_to_drop_ships

--- shipment_builder.rb
+++ shipment_builder.rb
@@ -30,15 +30,8 @@ class ShipmentBuilder
   end
 
   def build_shipments_by_ship_status
-    # MAGIC GOES HERE TO DO THE CONSOLIDATION
-
-    if in_stock_items.length > 0 && drop_ship_items.length > 0
-      # If we've got us some in_stocks and some drop_ships, let's see if
-      # we can do some consolidating. It only makes sense to consolidate if we can completely
-      # get rid of in_stocks.
-      if consolidate_to_drop_ships?
-        in_stock_items.each{|li| li.ship_status = :drop_ship}
-      end
+    if consolidate_to_drop_ships?
+      in_stock_items.each{|li| li.ship_status = :drop_ship}
     end
 
     line_items.each do |li|
@@ -48,11 +41,8 @@ class ShipmentBuilder
   end
 
   def consolidate_to_drop_ships?
-    in_stock_items.each do |in_stock_item|
-      matching_ds = drop_ship_items.any?{|drop_ship_item| drop_ship_item.vendor_id == in_stock_item.vendor_id}
-      in_stock_item.consolidatable = (matching_ds && in_stock_item.drop_shippable)
-    end
-    in_stock_items.all?(&:consolidatable)
+    return false unless in_stock_items.all?(&:drop_shippable)
+    (in_stock_items.map(&:vendor_id) - drop_ship_items.map(&:vendor_id)).empty?
   end
 
   def in_stock_items
